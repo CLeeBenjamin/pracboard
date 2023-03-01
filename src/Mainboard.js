4a7d21c60b39d803ab5467edf5bec475604ba020
Caston <castonboyd@gmail.com>
2023-03-01 17:59:26 -0500

    refactored

--- src/Mainboard.js
+++ src/Mainboard.js
@@ -3,8 +3,7 @@ import './index.css';
 
 
 function Mainboard() {
-  const board = [];
-  const [catPos, setCatPos] = useState([3,3]) // [x,y]
+  const [catPos, setCatPos] = useState([3,3]) 
 
   const handleClick = (i, j) => {
     console.log(`Square (${i}, ${j}) was clicked.`);
@@ -12,7 +11,7 @@ function Mainboard() {
 
   const handleKeyDown = (event) => {
     setCatPos(prevPos => {
-      let newPos = [...prevPos]; // create a copy of the previous position
+      let newPos = [...prevPos]; 
       switch (event.key) {
         case 'ArrowUp':
           newPos[0] = Math.max(newPos[0] - 1, 0);
@@ -33,38 +32,41 @@ function Mainboard() {
     });
   };
   
-
-  for (let i = 0; i < 7; i++) {
-    const row = [];
-    for (let j = 0; j < 7; j++) {
-      const squareKey = `${i}-${j}`;
-      const squareClass = `square ${(i + j) % 2 === 0 ? "light" : "dark"}`;
-      const isTopRight = i === 6 && j === 0;
-      const isBottomLeft = i === 0 && j === 6;
-      const center = i === catPos[1] && j === catPos[0]
-      const waterBowl = isTopRight ? <div className="water-bowl" onKeyDown={handleKeyDown} /> : null;
-      const scratchPad = isBottomLeft ? <div className="scratch-pad" /> : null;
-      const cat = center ? <div className="cat" />  : null;
-     
-      row.push(
-        <div
-          key={squareKey}
-          className={squareClass}
-          onClick={() => handleClick(i, j)}>
-          {waterBowl}
-          {scratchPad}
-          {cat}
-
-        </div>
-      );
-      
+  function createBoard() {
+    const board = [];
+    for (let i = 0; i < 7; i++) {
+      const row = [];
+      for (let j = 0; j < 7; j++) {
+        const squareKey = `${i}-${j}`;
+        const isTopRight = i === 6 && j === 0;
+        const isBottomLeft = i === 0 && j === 6;
+        const center = i === catPos[1] && j === catPos[0];
+        const squareClass = `square ${(i + j) % 2 === 0 ? "light" : "dark"}`;
+        const waterBowl = isTopRight ? <div className="water-bowl" onKeyDown={handleKeyDown} /> : null;
+        const scratchPad = isBottomLeft ? <div className="scratch-pad" /> : null;
+        const cat = center ? <div className="cat" /> : null;
+  
+        row.push(
+          <div
+            key={squareKey}
+            className={squareClass}
+            onClick={() => handleClick(i, j)}
+          >
+            {waterBowl}
+            {scratchPad}
+            {cat}
+          </div>
+        );
+      }
+      board.push(<div key={i} className="row">{row}</div>);
     }
-    board.push(<div key={i} className="row">{row}</div>);
+    return board;
   }
+  
 
   return (
     <div className="board" onKeyDown={handleKeyDown} tabIndex="0">
-      {board}
+      {createBoard()}
     </div>
   );
 }
